64bab485cc222883cca1588616efe7a9fb122779
Liân <liangrandon@gmail.com>
2022-04-15 15:50:38 -0400

    Create background.py

---
+++ background.py
@@ -0,0 +1,12 @@
+import pyxel
+
+class Star:
+    def __init__(self,x,y,radius,color,speed):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
+        self.speed = speed
+        
+        def Update(self):
+            pass
