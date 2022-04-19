a8f218204eee191c8f9fc230a3beda877053423f
Liân <liangrandon@gmail.com>
2022-04-18 20:13:38 -0400

    Player Wings n init Player Rocket

--- player/playerMain.py
+++ player/playerMain.py
@@ -7,6 +7,7 @@ class PlayerCore:
 
     def DrawPlayer(self):
         pyxel.blt(self.x,self.y,0,0,0,16,16,0)
+        PWings.DrawWings(self.x,self.y)
 
     def InputController(self):
         if pyxel.btn(pyxel.KEY_D):
@@ -18,4 +19,22 @@ class PlayerCore:
         if pyxel.btn(pyxel.KEY_S):
             self.y = self.y + 1
 
+class PlayerWings:
+    def __init__(self,sx,sy):
+        self.sx = sx
+        self.sy = sy
+        self.x = 12 #unused
+        self.y = 5
+        
+    def DrawWings(self,px,py):
+        pyxel.blt(12+px,self.y+py,0,self.sx,self.sy,8,8,0) #right
+        pyxel.blt(-4+px,self.y+py,0,self.sx,self.sy,-8,8,0) #left
+        
+class PlayerRocket:
+    def __init__(self):
+        pass
+
 Player = PlayerCore(50,50)
+PWings = PlayerWings(16,0)
+
+#16,0 to 23,7
